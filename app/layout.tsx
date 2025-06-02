9b0e04bdd8dbc0f6e1cbd96ccdc9212e755012df
Amon Vanderlei <amon.chalegre@hotmail.com>
2025-06-01 23:03:34 -0300

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Inter:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,61 @@
-import Image from "next/image";
+import Link from "next/link";
+import { BsGift } from "react-icons/bs";
+import { FiCheckSquare } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen flex flex-col items-center px-6 py-10">
+      <header className="w-full flex justify-between items-center mb-10">
+        <Link href="/" className="text-xl tracking-widest font-title">
+          PRESENTEIE-ME
+        </Link>
+        <Link
+          href="/signin"
+          className="border border-golden text-golden px-4 py-1 rounded-full text-sm hover:scale-105 transition-transform duration-200 ease-in-out
+"
+        >
+          Login/Cadastro
+        </Link>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="max-w-11/12 text-center items-center flex flex-col gap-10 mt-6">
+        <div className="flex flex-col gap-2 items-center">
+          <BsGift className="text-golden text-8xl" />
+
+          <h2 className="text-3xl md:text-4xl font-title font-semibold leading-snug">
+            Sua lista de desejos,
+            <br />
+            sua forma de ser lembrado.
+          </h2>
+
+          <p className="text-gray-700">
+            Crie sua lista de presentes e receba contribuições com carinho.
+          </p>
         </div>
+
+        <Link
+          href="/signin"
+          className="w-1/2 bg-golden text-white py-3 px-6 rounded-md font-medium my-6 hover:opacity-90 hover:scale-105 transition-transform duration-200 ease-in-out"
+        >
+          Criar minha lista
+        </Link>
+
+        <ul className="text-left space-y-4">
+          <li className="flex items-center gap-2">
+            <FiCheckSquare /> Sem complicações
+          </li>
+          <li className="flex items-center gap-2">
+            <FiCheckSquare /> Ideal para qualquer ocasião
+          </li>
+          <li className="flex items-center gap-2">
+            <FiCheckSquare /> Compartilhe com um link
+          </li>
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="w-11/12 mt-20 text-sm text-gray-800 flex gap-4 flex-wrap justify-evenly">
+        <a href="https://github.com/AmonVanderlei/presenteie-me" target="_blank">Saiba Mais</a>
+        <a href="https://github.com/AmonVanderlei" target="_blank">Feito por Amon Vanderlei</a>
       </footer>
     </div>
   );
